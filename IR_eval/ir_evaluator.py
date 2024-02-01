c20b6ee66f75e4d47500fa504fc7d0a2044854b6
JacopoLungo <jacopo.lungo@hotmail.it>
2024-02-01 15:07:32 +0100

    small changes in db creation

--- IR_eval/ir_evaluator.py
+++ IR_eval/ir_evaluator.py
@@ -24,7 +24,7 @@ class IrEvaluator:
                 repo_user_prj, #example: 'reingart/pyafipws'
                 embedder = 'charbert', #charbert or bert
                 root_git_clone = '/content/drive/MyDrive/PoliTo/NLP_Polito/progetto/git_clones/',
-                
+                persist_db_path = '/content/drive/MyDrive/PoliTo/NLP_Polito/progetto/chromaDbs'
                 ):
 
 
@@ -32,10 +32,13 @@ class IrEvaluator:
         self.root_git_clone = Path(root_git_clone)
         self.repo_user_prj = repo_user_prj
         self.prj_name = repo_user_prj.split('/')[-1]
-        self.repo_path = root_git_clone / self.prj_name
+        self.repo_path = self.root_git_clone / self.prj_name
+        
+        self.persist_db_path = Path(persist_db_path) / self.prj_name
         
         self.llm = None
 
+        #create the repo folder if not exists
         if not os.path.exists(self.repo_path):
             self.repo_path.mkdir()
             print(f'Downloading the repo at {self.repo_path}')
@@ -43,7 +46,6 @@ class IrEvaluator:
         else:
             print(f'Repo already downloaded at {self.repo_path}')
 
-        
         #Create the sentence Transformer
         if embedder == 'bert':
             self.embeddings = HuggingFaceEmbeddings(model_name_or_path = 'bert-base-uncased')
@@ -58,28 +60,35 @@ class IrEvaluator:
             self.embeddings = CharBertEmbeddings(sentTrans)
         
         
-    def create_db(self, chunk_size = 2000, chunk_overlap = 200):
-        #Create the loader
-        loader = GenericLoader.from_filesystem(
-                path = self.repo_path,
-                glob="**/*",
-                suffixes=[".py"], #puoi scegliere quali file includere
-                #exclude = ["**/non-utf8-encoding.py"], #quali file escludere
-                parser=LanguageParser(language=Language.PYTHON, parser_threshold=0),
-                #nel parser puoi selezionare il linguaggio e il numero di righe minime che vengono prese per ogni blocco
-                )
+    def create_db(self, chunk_size = 2000, chunk_overlap = 200, load_db = False, persist_db = False):
+        if load_db:
+            self.db= Chroma(persist_directory = str(self.persist_db_path), embedding_function = self.embeddings)
+            print('Loaded Db with', len(self.db.get()['ids']), 'chunks')
         
-        #load the data
-        docs = loader.load()
-        print(f'Loader: {len(docs)} chunks')
+        else:
+            #Create the loader
+            loader = GenericLoader.from_filesystem(
+                    path = self.repo_path,
+                    glob="**/*",
+                    suffixes=[".py"], #puoi scegliere quali file includere
+                    #exclude = ["**/non-utf8-encoding.py"], #quali file escludere
+                    parser=LanguageParser(language=Language.PYTHON, parser_threshold=0),
+                    #nel parser puoi selezionare il linguaggio e il numero di righe minime che vengono prese per ogni blocco
+                    )
+            
+            #load the data
+            docs = loader.load()
+            print(f'Loader: {len(docs)} chunks')
 
-        python_splitter = RecursiveCharacterTextSplitter.from_language(
-            language=Language.PYTHON, chunk_size = chunk_size, chunk_overlap = chunk_overlap
-            )
-        texts = python_splitter.split_documents(docs)
-        print(f'Splitter: {len(texts)} chunks')
-        
-        self.db = Chroma.from_documents(texts, self.embeddings)
+            python_splitter = RecursiveCharacterTextSplitter.from_language(
+                language=Language.PYTHON, chunk_size = chunk_size, chunk_overlap = chunk_overlap
+                )
+            texts = python_splitter.split_documents(docs)
+            print(f'Splitter: {len(texts)} chunks')
+            if persist_db:
+                self.db = Chroma.from_documents(texts, self.embeddings, persist_directory = str(self.persist_db_path))
+            else:
+                self.db = Chroma.from_documents(texts, self.embeddings)
     
     def create_retriever(self, k = 5, search_type = "mmr"):
         #u can create different retrievers without having to recreate the db
@@ -107,8 +116,17 @@ class IrEvaluator:
         pass
         return None
         
-    def evaluate_retriever_via_df(self, code_query = False): #TODO vedere se si può passare da qui similarity e k
+    def evaluate_retriever_via_df(self, code_query = False, k = None, search_type = None):
+        #you can pass here search type and k to avoid calling create_retriever to modify the retriever
+        # however if must be called before this method the first time
+        
+        if k is not None:
+            self.retriever.search_kwargs["k"] = k
+        if search_type is not None:
+            self.retriever.search_type = search_type
+            
         k = self.retriever.search_kwargs["k"]
+        
         num_total = len(self.df)
         
         summary = {
@@ -130,7 +148,7 @@ class IrEvaluator:
                 query = self.generate_code_query(query)
                 
             hits = self.retriever.get_relevant_documents(query)
-            preds = [hits[i].metadata['source'].replace(self.repo_path + '/', '') for i in range(k)]
+            preds = [hits[i].metadata['source'].replace(str(self.repo_path) + '/', '') for i in range(k)]
             #print(f'{preds = }')
             #print(f'{row.path=}')
             try:
@@ -139,8 +157,8 @@ class IrEvaluator:
                 for j in range(match_idx, k):
                     summary['top'+str(j+1)] += 1/num_total
             except:
-                print(f'{preds = }')
-                print(f'{row.path=}\n\n')
+                #print(f'{preds = }')
+                #print(f'{row.path=}\n\n')
 
                 summary['wrong_idx'].append(idx)
             
